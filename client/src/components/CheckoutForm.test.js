27684c86836e62bef6b0d0700e547617435092a0
brennanlawrence <brennanlawrence2002@gmail.com>
2020-12-04 10:06:43 -0700

    Completed Challenges

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,31 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from "@testing-library/user-event";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render( <CheckoutForm/> );
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.getByText(/checkout form/i);
+
+    expect(header).toBeInTheDocument();
+});
+
+test("form shows success message on submit with form details", () => {
+    render( <CheckoutForm/> );
+
+    const firstNameInput = screen.getByLabelText( /first name/i );
+    const button = screen.getByRole( "button" );
+    
+    userEvent.type( firstNameInput, "Dave" );
+    userEvent.click( button );
+
+
+    const message = screen.getByText( /you have ordered some plants!/i );
+
+    expect( message ).toBeInTheDocument();
+
+});
